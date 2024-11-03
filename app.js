db7b5cbc9aae6408d3aee60c56b702f1a0e740c3
mlpz2 <miguelkrasnob@gmail.com>
2024-11-03 21:32:50 +0100

    Agregado archivo app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+clearTimeoutonst express = require('express')
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+
+
+const app = express();
+
+const port = 3000;
+
+
+app.use(cors());
+
+app.use(bodyParser.json());
